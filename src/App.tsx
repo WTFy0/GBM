0140548640f2f75c80bca1bdccde140bc69171f4
WTFy0 <86792771+WTFy0@users.noreply.github.com>
2025-08-24 15:19:21 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './contexts/LanguageContext';
-import ScrollToTop from './components/UI/ScrollToTop';
+
 import ScrollToTop from './components/UI/ScrollToTop';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
