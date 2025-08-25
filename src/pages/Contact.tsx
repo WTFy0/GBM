388701285630014b5c5879346f4443b857296f25
WTFy0 <86792771+WTFy0@users.noreply.github.com>
2025-08-25 15:42:57 +0300

    Updated Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -216,7 +216,7 @@ const Contact: React.FC = () => {
     >
       {/* Google Maps Embed */}
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.2209!2d31.2919!3d30.0090!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583e2e4fffb4c7%3A0x123456789!2sAl%20Mokattam%2C%20Cairo!5e0!3m2!1sen!2seg!4v1693300000000!5m2!1sen!2seg"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.2209!2d31.336839390406368!3d29.983243183464438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583e2e4fffb4c7%3A0x123456789!2sAl%20Mokattam%2C%20Cairo!5e0!3m2!1sen!2seg!4v1693300000000!5m2!1sen!2seg"
         width="100%"
         height="100%"
         style={{ border: 0 }}
